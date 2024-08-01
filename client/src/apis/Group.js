5d1db7444f4f4a33cb233a7535e0f535983f52fd
Anmol Patel <120417559+Anmolpatel562@users.noreply.github.com>
2024-08-02 03:01:15 +0530

    fixed

--- client/src/apis/Group.js
+++ client/src/apis/Group.js
@@ -5,7 +5,7 @@ const backendUrl = process.env.REACT_APP_BACKENDURL;
 
 export const getAllGroupsByDeviceId = async (device) => {
     try{
-       const response = await axios.get(`${backendUrl}group/getAllGroupsByDeviceId/${device}`,);
+       const response = await axios.get(`${backendUrl}group/getAllGroupsByDeviceId/${device}`);
        return response.data.groups;
     }catch(error){
         console.log(error);
